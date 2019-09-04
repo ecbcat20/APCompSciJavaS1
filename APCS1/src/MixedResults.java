8db1e72ac802a7a1cf516b0c3c611fcf8cdc624c
184163 <184163@427-NA307-018.mcpsmd.org>
2019-09-04 12:51:51 -0400

    Added more

---
+++ APCS1/src/MixedResults.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class MixedResults {
+
+	
+	static double d1 = 37.9; //Initialize these variables at the top of your program
+	static double d2 = 1004.128;
+	static int i1 = 12; 
+	static int i2 = 18;
+
+	static int selection;
+
+	static Scanner scannerFunctionSelector = new Scanner(System.in); //open scanner
+	static int count = 0; //initialize counter for selectorTool()
+
+
+	//case 1
+	public static void testerFunction1() {
+
+		// 57.2 x (i1 / i2) + 1
+		System.out.println((57.2) * (i1/i2) + 1);
+	}
+
+	//case 2
+	public static void testerFunction2() {
+
+		//same formula except cast i1 as double
+		double i1d = i1;
+		System.out.println((57.2) * i1d/i2 + 1);
+
+	}
+	
+	//case 3
+	public static void testerFunction3() {
+
+		//15 – i1 x (d1 x 3) + 4
+		System.out.println((15) - (i1 * (d1 * 3)) + 4);
+
+	}
+
+	//case 4
+	public static void testerFunction4() {
+
+		//same formula except cast product of  d1x3 as integer
+		int productOf3d = (int) (d1*3);
+		System.out.println((15) - (i1 * (productOf3d)) + 4);
+
+	}
+
+	//case 5
+	public static void testerFunction5() {
+
+		//same formula except cast d1 as integer
+		int d1d = (int) d1;
+		System.out.println((15) - (i1 * (d1d * 3)) + 4);
+
+	}
+
+
+	public static void main(String[] args) {
+
+		//run 5 times
+		do {
+			MixedResultsSelection.main(args);
+		} while(count <= 4);
+
+	}
+}
+
---
+++ APCS1/src/MixedResultsSelection.java
@@ -0,0 +1,37 @@
+
+public class MixedResultsSelection {
+
+	public static void main(String[] args) {
+		//tool for selecting which case to run
+
+		System.out.println("Make Your Selection, 1-5: ");
+		MixedResults.selection = MixedResults.scannerFunctionSelector.nextInt(); //scanner for getting integer input
+
+		switch (MixedResults.selection) { //switch for selecting which method
+
+			case 1: MixedResults.testerFunction1();
+			MixedResults.count++; //increase counter
+			break;
+	
+			case 2: MixedResults.testerFunction2();
+			MixedResults.count++;
+			break;
+	
+			case 3: MixedResults.testerFunction3();
+			MixedResults.count++;
+			break;
+	
+			case 4: MixedResults.testerFunction4();
+			MixedResults.count++;
+			break;
+	
+			case 5: MixedResults.testerFunction5();
+			MixedResults.count++;
+			break;
+	
+			default: System.out.println("You Didn't Make a Selection!: "); //default action; loop back
+
+		}
+	}
+}
+
