80868053ee34fbcd33c9b14d3775195a9feb496d
Shy <ecbcat20@gmail.com>
2019-09-08 12:04:10 -0400

    added comments to APLine.java

--- APCS1/src/APLine.java
+++ APCS1/src/APLine.java
@@ -1,31 +1,35 @@
 
 public class APLine {
 
+	//initialize variables
 	int a, b, c;
 	int x, y;
 	double slope;
 	boolean isOnLine;
 
+	// constructor for APLine
 	public APLine(int a, int b, int c) {
 		this.a = a; this.b = b; this.c = c;
 	}
 
+	//getSlope() method
 	double getSlope() {	
 		return slope = ((-1*b)/a);
 
 	}
 
+	//isOnLine() method
 	boolean isOnLine(int x, int y) {
 		this.x = x; this.y = y;
-		
-		if (a*x + b*y == 0) {
+
+		if (a*x + b*y == 0) { //formula for finding if equal 0
 			isOnLine = true;
 			System.out.println(isOnLine);
-			return isOnLine;
+			return isOnLine; //return true
 		} else {
 			isOnLine = false;	
 			System.out.println(isOnLine);
-			return isOnLine;
+			return isOnLine; //return false
 		} 
 
 	}
@@ -34,18 +38,25 @@ public class APLine {
 	public static void main(String[] args) {
 
 
-		APLine line1 = new APLine(5, 4, -17);
-		double slope1 = line1.getSlope();
-		line1.isOnLine(5, -2);
+		APLine line1 = new APLine(5, 4, -17); //create new line1
+		line1.getSlope(); //get slope of line1
+		line1.isOnLine(5, -2); //calculate if points are on line1
 
-		APLine line2 = new APLine(-25, 40, -17);
-		double slope2 = line2.getSlope();
-		line2.isOnLine(5, -2);
+		APLine line2 = new APLine(-25, 40, -17); //create new line2
+		line2.getSlope(); //get slope of line2
+		line2.isOnLine(5, -2); //calculate if points are on line2
 
-		System.out.println(slope1);
-		System.out.println(slope2);
+		System.out.println(line1.slope); //print slope of line1
+		System.out.println(line2.slope); //print slope of line2
 
 
+		//output of program
+		
+		/* false
+		false
+		0.0
+		1.0
+		*/
 
 
 
