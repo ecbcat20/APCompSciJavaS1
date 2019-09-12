11741569b7aa548a414d9d2be256b43dd47f3912
Shy <ecbcat20@gmail.com>
2019-09-12 16:24:52 -0400

    Finished MultipleMath.java

--- APCS1/src/challenges/MultipleMath.java
+++ APCS1/src/challenges/MultipleMath.java
@@ -3,64 +3,68 @@ package challenges;
 import java.util.Scanner;
 
 public class MultipleMath {
-	
-	static double fIn;
-	static int a;
-	static int b;
+
+	//initialize variables
+	static float fIn; //temperature in
+	static int a; //a in pythagoras
+	static int i = 1; // constant
+	static int b; //b in pythagoras
+	static double rad; //radius in volume
+ 
+	//initialize equation variables
 	static String equation;
 	int equationSelected;
-	static double rad;
-	
-	static Scanner scanner = new Scanner(System.in);
-	
-	@SuppressWarnings("static-access")
-	public MultipleMath(int fIn, int a, int b, String equation) {
-		this.fIn = fIn; this.a = a; this.b = b; this.equation = equation;
+
+	static Scanner scanner = new Scanner(System.in); //open scanner
+
+	//temperature calculator
+	public static void temperature() {
+		System.out.println((5*(fIn - 32)/9));
 	}
-	
-	static double temperature() {
-		double temperature = ((5/9) * (fIn + (-32)));
-		System.out.println(temperature);
-		return temperature;
-	}
-	
-	static double volume() {
+
+	//volume calculator
+	public static void volume() {
 		double volume = ((4/3) * Math.PI * (rad * rad * rad));
 		System.out.println(volume);
-		return volume;
 	}
-	
-	static double pythagoras(int a, int b) {
+
+	//Pythagorean theorem calculator for c
+	public static void pythagoras(int a, int b) {
 		double pythagoras = (Math.sqrt((a*a) + (b*b)));
 		System.out.println(pythagoras);
-		return pythagoras;
 	}
-	
-	
 
+	//main method
 	public static void main(String[] args) {
-		
-		System.out.println("Please enter your equation: (Temperature, Volume, Pythagoras: ");
-		equation = scanner.nextLine();
-		
-		if(equation.equalsIgnoreCase("Temperature") ) {
-			System.out.println("Enter your Fahrenheit Value");
-			fIn = scanner.nextDouble();
-			temperature();
-		} else if(equation.equalsIgnoreCase("Volume")) {
-			rad = scanner.nextInt();
-			volume();
-		} else if(equation.equalsIgnoreCase("Pythagoras")) {
-			System.out.println("Enter your value for a: ");
-			a = scanner.nextInt();
-			System.out.println("Enter your value for b: ");
-			b = scanner.nextInt();
-			pythagoras(a, b);
-		}
-		
-  
-		
+
+		do { //do-while loop
+
+			System.out.println("Please enter your equation: (Temperature, Volume, Pythagoras: ");
+			equation = scanner.nextLine();
+
+			if(equation.equalsIgnoreCase("Temperature") ) {
+				System.out.println("Enter your Fahrenheit Value");
+				fIn = (float) scanner.nextDouble();
+				temperature();
+
+			} else if(equation.equalsIgnoreCase("Volume")) {
+				rad = scanner.nextInt();
+				volume();
+
+			} else if(equation.equalsIgnoreCase("Pythagoras")) {
+				System.out.println("Enter your value for a: ");
+				a = scanner.nextInt();
+				System.out.println("Enter your value for b: ");
+				b = scanner.nextInt();
+				pythagoras(a, b);
+			} else {
+				System.out.println("No valid input: ");
+			}
+
+			scanner.close(); //close scanner
+
+		} while (i == 1); //always
+
 	}
 
 }
- 
