782f144ebf264caf3a3f90e6562aa2b58f08a793
Shy <ecbcat20@gmail.com>
2019-09-12 16:26:19 -0400

    Removed unused import in FrameCreator.java

--- APCS1/src/swingcalculator/FrameCreator.java
+++ APCS1/src/swingcalculator/FrameCreator.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class FrameCreator {
 
