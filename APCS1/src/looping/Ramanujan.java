b9882f41f82e2b6b5376b26e14b43a118d5e10e9
184163 <184163@427-NA307-018.mcpsmd.org>
2019-09-18 12:51:56 -0400

    Finished work on factorial program Also started work on Ramanujan.java

---
+++ APCS1/src/looping/Ramanujan.java
@@ -0,0 +1,48 @@
+package looping;
+
+public class Ramanujan {
+
+	int one, two, result;
+	int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8 , 9, 10};
+	
+	//1729 
+	//smallest number expressible as sum of two cubes
+	
+	//a^3 + b^3 = c^3 + d^3
+	//four nested (for) loops
+	
+	//print all integers less than or equal to n that can be expressed as the sum of two cubes
+	//two different ways
+	
+	public Ramanujan(int one, int two) {
+		this.one = one; this.two = two;
+	}
+	
+	//would need methods to determine how to break up number
+	//break up number by cubes
+	
+	int math() {
+		for(int i = 1; i <= 3; i++) {
+			if(one % numbers[2]) {
+				
+			}
+			for(int j = 1; j<=4; j++) {
+				for(int k = 1; k<=5; k++) {
+					for(int m = 1; m<=6; m++) {
+						
+					}
+				}
+			}
+		}
+		
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
