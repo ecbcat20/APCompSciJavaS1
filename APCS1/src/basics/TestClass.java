3062793278467df1e5457ccf903ee00f8bfb7641
184163 <184163@427-NA307-018.mcpsmd.org>
2019-09-19 12:48:16 -0400

    Started TestClass.java

---
+++ APCS1/src/basics/TestClass.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class TestClass {
+
+	public static void main (String[] args) {
+		for(int j = 1; j <= 3; j++) {
+			for(int k = 1; k < 4; k++) {
+				System.out.println(j*k + " ");
+			}
+		}
+	}
+}
