ae580c683aa60dc842e85ceb6c8ad17061cbec89
Shy <ecbcat20@gmail.com>
2019-09-19 18:00:02 -0400

    Continued work on StarPatterns for X star

--- APCS1/src/looping/StarPatterns.java
+++ APCS1/src/looping/StarPatterns.java
@@ -10,14 +10,35 @@ public class StarPatterns {
 		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();		
 
+		//may have to implement a while loop to finish
+
+		/* on first line while x is equal to i, print i spaces
+		 * after i spaces is printed, print one *
+		 */
+
 		for(int i = 0; i<=number; i++) { //master for loop, open
 
 			for(int j = i; j>0; j--) { //i = j, j is greater than 0, decrement j
 				System.out.print(" "); //will print spaces
 
+				/*for(int x = number; x>0; x--) {
+					System.out.print("*");
+				}
+				
+				System.out.println(" ");
+				*/
+				
+				
+				/*while(j > 0) {
+					System.out.print(" ");
+				}
+
+				 */
+
+
 			} //close second for loop
 
-			System.out.println("*"); //print star
+			System.out.println(i); //print star
 
 		} //close first for loop
 
