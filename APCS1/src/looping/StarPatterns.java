c9df1c148d0bed9f2f76e2fb5a98d850b07f6795
Shy <ecbcat20@gmail.com>
2019-09-19 17:06:36 -0400

    Finished one-half of the java x

--- APCS1/src/looping/StarPatterns.java
+++ APCS1/src/looping/StarPatterns.java
@@ -8,24 +8,37 @@ public class StarPatterns {
 
 		System.out.println("Enter your odd number: ");
 		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
+		int number = scanner.nextInt();		
 
+		for(int i = 0; i<=number; i++) { //master for loop, open
 
-		for (int i=0; i<=number; i++) {
-			for(int j = number; j<=0; j--) { //print spaces
+			for(int j = i; j>0; j--) { //i = j, j is greater than 0, decrement j
+				System.out.print(" "); //will print spaces
 
-				System.out.print("*");
-			
+			} //close second for loop
 
+			System.out.println("*"); //print star
+
+		} //close first for loop
+
+		//----------------------------------------------------------------------------------
+
+		for(int m = (number - 1); m>=0; m--) { //open new for loop that runs same amount of times
+
+			for(int n = m; n>0; n--) {
+				System.out.print(" ");
+				for(int o = n; o<0; o++) {
+					System.out.print("?");
+				}
 			}
-			
-			System.out.print("*");
-			System.out.println();
-
-
-			scanner.close();
-
+			System.out.println("*");
 		}
+		System.out.println();
+
+		scanner.close();
+
 
 	}
 }
+
+
