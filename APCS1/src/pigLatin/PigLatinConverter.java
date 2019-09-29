0682639e8b5b1e1c9fea695ab414fe48a37e3449
Shy <ecbcat20@gmail.com>
2019-09-29 16:58:12 -0400

    Finished PigLatinConverter.java

--- APCS1/src/pigLatin/PigLatinConverter.java
+++ APCS1/src/pigLatin/PigLatinConverter.java
@@ -2,8 +2,9 @@ package pigLatin;
 
 public class PigLatinConverter {
 
-	String input;
+	static String input, finalPigLatin;
 
+	@SuppressWarnings("static-access")
 	public PigLatinConverter(String input) {
 		this.input = input;
 	}
@@ -24,22 +25,61 @@ public class PigLatinConverter {
 			}
 		}
 
-
-
 		if(index == -1) //if vowels are at beginning of string, set to -1
 			return "-1";
 
-		String finalPigLatin = input.substring(index)  //return final result
-				+ input.substring(0, index) + "ay"; 
+		//need to find the character at location 0 after conversion and change case;
 
+		input.substring(0).toUpperCase();
+
+
+
+		//take what you got and capitalize the first letter
+		if(input.indexOf("A") == 0 || input.indexOf("A") == 0 ||input.indexOf("A") == 0 
+				|| input.indexOf("A") == 0 ||input.indexOf("B") == 0 ||
+				input.indexOf("C") == 0 ||input.indexOf("D") == 0 ||input.indexOf("E") == 0 ||
+				input.indexOf("F") == 0 ||input.indexOf("G") == 0 ||input.indexOf("H") == 0 ||
+				input.indexOf("I") == 0 ||input.indexOf("J") == 0 ||input.indexOf("K") == 0 ||
+				input.indexOf("L") == 0 ||input.indexOf("M") == 0 ||input.indexOf("N") == 0 ||
+				input.indexOf("O") == 0 ||input.indexOf("P") == 0 ||input.indexOf("Q") == 0 ||
+				input.indexOf("R") == 0 ||input.indexOf("S") == 0 ||input.indexOf("T") == 0 ||
+				input.indexOf("U") == 0 ||input.indexOf("V") == 0 ||input.indexOf("W") == 0 ||
+				input.indexOf("X") == 0 || input.indexOf("Y") == 0 ||input.indexOf("Z") == 0) {
+
+			finalPigLatin = input.substring(index)
+					+ input.substring(0, index) + "ay"; //return final result
+
+			finalPigLatin = finalPigLatin.substring(0,1).toUpperCase() + 
+					finalPigLatin.substring(1).toLowerCase();
+
+			return finalPigLatin;
+
+
+		} else if(input.indexOf("q") == 0 && input.indexOf("u") == 1 || 
+				input.indexOf("y") == 0) {
+			finalPigLatin = input.substring(index)
+					+ input.substring(0, index) + "ay"; //return final result
+
+
+			return finalPigLatin;
+			
+		} else {
+			
+			finalPigLatin = input.substring(index)
+					+ input.substring(0, index) + "ay"; //return final result
+		}
+
+		//at this point finalPigLatin is properly capitalized.
+
+		
 		return finalPigLatin;
 	}
+}
 
 
 
 
 
-} 
 
 
 
