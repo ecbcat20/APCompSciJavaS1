b7e83583f5f7a731397f43b44351500ade12b154
184163 <184163@427-ZIMC-558.mcpsmd.org>
2019-10-08 11:43:15 -0400

    Studied for exam

--- APCS1/src/basics/StringCompare.java
+++ APCS1/src/basics/StringCompare.java
@@ -6,14 +6,32 @@ public class StringCompare {
 	static String str2 = "Hi";
 	//-----------------------------\\
 	static String h = "New";
-	static String i = "NewString";
-	
+	static String i = "MewString";
+
+	//-----------------------------\\
+	static String x =  "Hello";
+	static String y = "Hello World";
+
+	//-----------------------------\\
+
+	static String u = new String("BathTUB");
+	static String w = new String("bathtub");
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+		//Hello
+		//Hi
+
+		int two = h.compareTo(i);
+		System.out.println(two);
+		/*
 		int num = h.compareTo(i); 
 		System.out.println(num);
-		
+		 */
+
 	}
 
 }
+
