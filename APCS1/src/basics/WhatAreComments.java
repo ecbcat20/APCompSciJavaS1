5691ddf8e1a79ba7455207b2754cec6a2a435fe4
Skye <ecbcat20@gmail.com>
2019-10-13 17:13:29 -0500

    Demonstrating comments and error catching.

---
+++ APCS1/src/basics/WhatAreComments.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class WhatAreComments {
+
+	public static void main(String[] args) {
+
+		int num = 5;
+		int num2 = 0;
+
+		try {
+			System.out.println(num/num2);
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+	}
+
+}
