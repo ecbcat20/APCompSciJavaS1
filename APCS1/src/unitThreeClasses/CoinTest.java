3427cca5720f83c62df8eb4b61da712f60327dbf
184163 <184163@427-NA307-018.mcpsmd.org>
2019-10-15 12:18:15 -0400

    Finished flipping coin random number of times.

--- APCS1/src/unitThreeClasses/CoinTest.java
+++ APCS1/src/unitThreeClasses/CoinTest.java
@@ -1,5 +1,7 @@
 package unitThreeClasses;
 
+import java.util.Random;
+
 public class CoinTest {
 
 	public static void main(String[] args) {
@@ -24,6 +26,21 @@ public class CoinTest {
 		bCoin.flip();
 
 		System.out.println("The value of bCoin is: " + bCoin.toString());
+
+		// generate a random number between 1 and 10.
+		// flip bCoin that many times.
+		// each time outpt its state after flip.
+
+		Random rand = new Random();
+		int min = 1, max = 10;
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+
+		for (int i = 0; i < randomNum; i++) {
+			bCoin.flip();
+			System.out.println(bCoin.toString());
+
+		}
+
 	}
 
 }
