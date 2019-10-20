22e2ddfa7e4f6fda80e90e45176b2a1515ac449a
Shy <ecbcat20@gmail.com>
2019-10-20 12:23:22 -0400

    Finished Bank Account 1

--- APCS1/src/unitThreeClasses/BankAccount.java
+++ APCS1/src/unitThreeClasses/BankAccount.java
@@ -11,19 +11,23 @@ public class BankAccount {
 		name = "guest";
 	}
 
+	// secondary constructor
 	public BankAccount(double balance, String name) {
 		this.balance = balance;
 		this.name = name;
 	}
 
+	// deposit
 	public void deposit(double amount) {
 		balance += amount;
 	}
 
+	// withdraw
 	public void withdraw(double amount) {
 		balance -= amount;
 	}
 
+	// print name and balance
 	public String toString() {
 
 		String everything = "Hello, " + name + " Your balance is: " + balance + ".";
