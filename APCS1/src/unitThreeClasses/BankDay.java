162c12b27e86a0359953dd55f8dbd28f1ed0c66c
Shy <ecbcat20@gmail.com>
2019-10-21 18:24:37 -0400

    Making Progress

--- APCS1/src/unitThreeClasses/BankDay.java
+++ APCS1/src/unitThreeClasses/BankDay.java
@@ -3,20 +3,31 @@ package unitThreeClasses;
 public class BankDay {
 
 	String date = "October 20, 2019";
-	private Banker banker;// instantiate
+	Banker banker;// instantiate
 
 	public BankDay(String date, Banker b, String[] accounts) { // pass
 		this.date = date;
 		this.banker = b; // declare
 	}
 
+	public BankDay() {
+		date = "October 20, 2019";
+	}
+
 	public String day() {
 		return date;
 	}
 
-	public BankAccountBanker search("bank_account_2013.txt") {
-
+	public BankAccountBanker search(String s) {
 		return account;
+	}
+
+	public void setBanker(Banker b) {
+		banker = b;
+	}
+
+	public Banker getBanker() {
+		return banker;
 	}
 
 }
---
+++ APCS1/src/unitThreeClasses/Launch.java
@@ -0,0 +1,13 @@
+package unitThreeClasses;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		
+		BankDay day = new BankDay();
+		day.day();
+		
+		Banker b = new Banker()
+	}
+
+}
