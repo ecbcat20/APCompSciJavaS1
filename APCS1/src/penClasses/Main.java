6b43e27f54f710e176c8647425d2426b9fb38814
Shy <ecbcat20@gmail.com>
2019-11-03 19:31:34 -0500

    Properly documented Pen Classes for reference later

--- APCS1/src/penClasses/Main.java
+++ APCS1/src/penClasses/Main.java
@@ -4,17 +4,28 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// instantiate new pen object
 		Pen p = new Pen();
+
+		// set pen color
 		p.setColor("blue");
+
+		// set pen type
 		p.setType("gel");
+
+		// set pen point
 		p.setPoint(7);
-		p.click();
 
+		// click pen
+		p.click(); // true
+
+		// click 4 times: false, true, false, true
 		System.out.println(p.click());
 		System.out.println(p.click());
 		System.out.println(p.click());
 		System.out.println(p.click());
 
+		// output pen and its attributes
 		System.out.println(p.toString());
 	}
 
--- APCS1/src/penClasses/Pen.java
+++ APCS1/src/penClasses/Pen.java
@@ -7,37 +7,44 @@ public class Pen {
 	private String color;
 	private boolean isClicked;
 
+	// default constructor
 	public Pen() {
 
 	}
 
+	// secondary constructor
 	public Pen(String type, int point, String color, boolean isClicked) {
 		this.type = type;
 		this.color = color;
 		this.isClicked = false;
 	}
 
+	// click pen
 	public boolean click() {
 
 		isClicked = !isClicked;
 		return isClicked;
 	}
 
+	// set color of pen
 	public void setColor(String c) {
 
 		color = c;
 	}
 
+	// set size of pen tip
 	public void setPoint(int p) {
 
 		point = p;
 	}
 
+	// set type of pen
 	public void setType(String t) {
 
 		type = t;
 	}
 
+	// print pen and its attributes
 	public String toString() {
 		return ("Type " + type + " Point: " + point + " Color: " + color + " " + " Is Clicked? " + isClicked);
 	}
