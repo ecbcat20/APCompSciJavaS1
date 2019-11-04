66fdc0244a3444e630675cc4475a5a00cb2e218a
Shy <ecbcat20@gmail.com>
2019-11-03 19:29:00 -0500

    Fixed Pen classes

--- APCS1/src/penClasses/Main.java
+++ APCS1/src/penClasses/Main.java
@@ -3,20 +3,18 @@ package penClasses;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Pen p = new Pen();
 		p.setColor("blue");
 		p.setType("gel");
 		p.setPoint(7);
 		p.click();
-		
+
 		System.out.println(p.click());
 		System.out.println(p.click());
 		System.out.println(p.click());
 		System.out.println(p.click());
-		
-		
-		
+
 		System.out.println(p.toString());
 	}
 
