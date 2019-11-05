5d1a89ba66403b38d4cd6c7c5b4e91f20650f58c
Shy <ecbcat20@gmail.com>
2019-11-05 17:23:28 -0500

    Fixed potential typo issues

--- APCS1/src/musicControl/Launch.java
+++ APCS1/src/musicControl/Launch.java
@@ -3,7 +3,7 @@ package musicControl;
 public class Launch {
     public static void main(String[] args) {
 
-        MP3 songList1 = new MP3(); // create new songlist
+        MP3 songList1 = new MP3(); // create new song list
         songList1.addSong("Beautiful People"); // add song
         songList1.addSong("Eastside");
         songList1.addSong("Parachute");
