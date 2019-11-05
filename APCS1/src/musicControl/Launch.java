627f254149d5b143fa337700a967c54901d9d020
Shy <ecbcat20@gmail.com>
2019-11-05 17:20:40 -0500

    Finished launch classes

--- APCS1/src/musicControl/Launch.java
+++ APCS1/src/musicControl/Launch.java
@@ -1,17 +1,18 @@
 package musicControl;
 
 public class Launch {
-    public static void main(String[] args) {
-        MP3 songList1 = new MP3();
-        songList1.addSong("Beautiful People");
-        songList1.addSong("Eastside");
-        songList1.addSong("Parachute");
+	public static void main(String[] args) {
 
-        songList1.returnList();
+		MP3 songList1 = new MP3();
+		songList1.addSong("Beautiful People");
+		songList1.addSong("Eastside");
+		songList1.addSong("Parachute");
 
-        songList1.removeSong("Beautiful People");
+		System.out.println(songList1.returnList());
 
-        songList1.returnList();
+		songList1.removeSong("Beautiful People");
 
-    }
+		System.out.println(songList1.returnList());
+
+	}
 }
