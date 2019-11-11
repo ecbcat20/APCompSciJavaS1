257d7365b0fb6ed53d28c02715e4e3f19009c4d1
Shy <ecbcat20@gmail.com>
2019-11-10 21:05:51 -0500

    Made framework for clickcounter

---
+++ APCS1/src/pointlessGUI/ClickCounter.java
@@ -0,0 +1,30 @@
+package pointlessGUI;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ClickCounter implements ActionListener {
+
+    JFrame frame = new JFrame("Click Counter Application");
+    JTextField textField = new JTextField(6);
+    JButton clickme = new JButton("Click Me!");
+
+
+    public ClickCounter() {
+
+    }
+
+    public void buildFrame() {
+        
+    }
+
+    public void setCounter() {
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
