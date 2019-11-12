b6b5c040116b553bdab48c97a58ecd376d8d0bb5
Shy <ecbcat20@gmail.com>
2019-11-11 20:36:07 -0500

    Continued work on ClickCounter classes and packages

--- APCS1/src/pointlessGUI/ClickCounter.java
+++ APCS1/src/pointlessGUI/ClickCounter.java
@@ -1,26 +1,30 @@
 package pointlessGUI;
+
 import javax.swing.*;
 import java.awt.event.*;
 
 public class ClickCounter implements ActionListener {
 
+    int num = 0;
     JFrame frame = new JFrame("Click Counter Application");
-    JTextField textField = new JTextField(6);
+    JPanel panel = new JPanel();
+    JLabel label = new JLabel("Button has been clicked " + num + " of Times.");
     JButton clickme = new JButton("Click Me!");
-
 
     public ClickCounter() {
 
     }
 
     public void buildFrame() {
-        
+        frame.setSize(100, 200);
+        panel.add(clickme);
+        panel.add(label);
+        panel.setVisible(true);
     }
 
     public void setCounter() {
 
     }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
