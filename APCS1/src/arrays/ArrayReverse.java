11206be5d19abfa5e552c153b9328c0e475db141
Skye <ecbcat20@gmail.com>
2019-11-21 12:45:55 -0500

    committed changes

--- APCS1/src/arrays/ArrayReverse.java
+++ APCS1/src/arrays/ArrayReverse.java
@@ -1,33 +1,35 @@
 package arrays;
 
+import java.util.Random;
+
 public class ArrayReverse {
 
-	public static void wrongReverse(int[] a) {
-		int[] b = new int[a.length];
-		
-		for (int i = b.length; i >= 0; i--) {
-			b[i] = a[i];
-		}
-		
-		System.out.println();
-		
-		for(int i:b) {
-			System.out.print(b[i] + ", ");
+	static int arr[] = new int[10];
+
+	public static void wrongReverse(int[] arr) {
+
+		int[] b = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			b[i] = arr[i];
+			System.out.println(b[i]);
 		}
 	}
-	
-	
 
 	public static void main(String[] args) {
-		double rand;
-		int[] arr = new int[10];
+		Random rand = new Random();
+		int r;
 
 		for (int i : arr) {
-			rand = Math.random() * 10;
-			arr[i] = (int) rand;
+			r = rand.nextInt((20 - 11) + 11);
+			arr[i] = r;
+			System.out.println(arr[i]);
 		}
-		
-		wrongReverse(arr);
+
+		System.out.println("\n");
+
+		for (int i : arr) {
+			System.out.println(arr[i]);
+		}
 
 	}
 
