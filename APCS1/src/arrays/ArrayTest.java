e4fa87355d53c9cf790705bad17d30ad75b5c41d
Skye <ecbcat20@gmail.com>
2019-12-02 20:50:25 -0500

    Added more for review purposes

---
+++ APCS1/src/arrays/ArrayTest.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 0, 1, 2, 3, 4, 5 };
+
+        // print stuff inside array
+        for (int i : arr) {
+            if (i < arr.length - 1) {
+                System.out.print(arr[i] + ", ");
+            } else {
+                System.out.print(arr[i]);
+            }
+
+        }
+
+        Integer[] arr2 = new Integer[] { 10, 3, 4, 5, 6, 67, 3, 2 };
+
+    }
+
+}
