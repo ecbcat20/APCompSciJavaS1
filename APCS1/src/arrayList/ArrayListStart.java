74755751d388698645aa0321f310528a092d3ccc
Shy <ecbcat20@gmail.com>
2019-12-05 20:17:40 -0500

    Finished first iteration of ArrayListStart

--- APCS1/src/arrayList/ArrayListStart.java
+++ APCS1/src/arrayList/ArrayListStart.java
@@ -1,22 +1,77 @@
 package arrayList;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayListStart {
 	static ArrayList<String> animals = new ArrayList<>();
 
-	
-	public static void addToList(ArrayList<String> arr, String s, int pos) {
-		arr.add(arr.get(pos));
-		arr.add(s);
+	public static void fillArrayList() {
+		animals.add("hamster");
+		animals.add(0, "cat");
+		animals.add(0, "bird");
+
+		Collections.sort(animals);
+
+		for (int i = 0; i < animals.size(); i++) {
+			if (animals.get(i).charAt(0) > "dog".charAt(0)) {
+				animals.add(i, "dog");
+			}
+		}
+
+		animals.add("iguana");
+		Collections.sort(animals);
+
+		for (int i = 0; i < animals.size(); i++) {
+			if (animals.get(i).charAt(0) > "fish".charAt(0)) {
+				animals.add(i, "fish");
+			}
+		}
+
+		for (int i = 0; i < animals.size(); i++) {
+			if (animals.get(i).charAt(0) > "ferret".charAt(0)) {
+				animals.add(i, "ferret");
+			}
+		}
+
+		animals.add("turtle");
+
+	}
+
+	public static void printAllElements() {
+
+		String[] animals2 = new String[animals.size()];
+		animals2 = (String[]) animals.toArray();
+
+		for (String s : animals2) {
+			System.out.print(s);
+		}
+	}
+
+	public static void printElements() {
+		System.out.println(animals.get(0));
+		System.out.println(animals.get(animals.size()));
+		System.out.println(animals.get(3));
+		System.out.println(animals.indexOf("fish"));
+	}
+
+	public static void removeElements() {
+		animals.remove(0);
+		animals.remove(animals.size());
+		animals.remove(5);
+
+		animals.set(animals.indexOf("dog"), "puppy");
+		animals.toString();
+		animals.clear();
+		animals.toString();
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		addToList("bird");
-		
+		fillArrayList();
+		animals.toString();
 
+		printAllElements();
+		printElements();
+		removeElements();
 	}
-
 }
