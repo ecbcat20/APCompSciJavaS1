c4eb525c2d02c0ccf0934568ef00846b32b18475
Shy <ecbcat20@gmail.com>
2019-12-08 19:10:51 -0500

    Properly commented on ArrayListStart

--- APCS1/src/arrayList/ArrayListStart.java
+++ APCS1/src/arrayList/ArrayListStart.java
@@ -10,51 +10,54 @@ public class ArrayListStart {
 		String cat = "cat";
 		String bird = "bird";
 		String hamster = "hamster";
-		
+
 		animals.add(hamster);
 		animals.add(0, cat);
 		animals.add(0, bird);
-		
+
 		System.out.println(animals);
 
+		// sort animals
 		Collections.sort(animals);
 
 		for (int i = 0; i < animals.size(); i++) {
 			if ((animals.get(i)).charAt(0) > "dog".charAt(0)) {
 				animals.add(i, "dog");
-				i = animals.size()-1;
+				i = animals.size() - 1;
 			}
 		}
-		
+
+		// after inserting dog using alphabet insert
 		System.out.println(animals);
 
 		animals.add("iguana");
 		Collections.sort(animals);
 
+		// add fish at proper position
 		for (int i = 0; i < animals.size(); i++) {
 			if (animals.get(i).charAt(0) > "fish".charAt(0)) {
 				animals.add(i, "fish");
-				i = animals.size()-1;
+				i = animals.size() - 1;
 			}
 		}
-		
+
 		System.out.println(animals);
 
 		for (int i = 0; i < animals.size(); i++) {
 			if (animals.get(i).charAt(0) > "ferret".charAt(0)) {
 				animals.add(i, "ferret");
-				i = animals.size()-1;
+				i = animals.size() - 1;
 			}
 		}
 
 		animals.add("turtle");
+		// print list after ferret and turtle were added
 		System.out.println(animals);
 
 	}
 
 	public static void printAllElements() {
-
-		
+		// print using for each
 		for (String s : animals) {
 			System.out.print(s + " ");
 		}
@@ -62,17 +65,17 @@ public class ArrayListStart {
 
 	public static void printElements() {
 		System.out.println(animals.get(0));
-		System.out.println(animals.get(animals.size()-1));
+		System.out.println(animals.get(animals.size() - 1));
 		System.out.println(animals.get(3));
 		System.out.println(animals.indexOf("fish"));
 	}
 
 	public static void removeElements() {
 		animals.remove(0);
-		animals.remove(animals.size()-1);
+		animals.remove(animals.size() - 1);
 		animals.remove(5);
 
-		animals.set(animals.indexOf("dog"), "puppy");
+		animals.set(animals.indexOf("dog"), "puppy"); // replace dog with puppy
 		System.out.println(animals);
 		animals.toString();
 		animals.clear();
@@ -86,7 +89,6 @@ public class ArrayListStart {
 		printElements();
 		System.out.println(animals);
 		removeElements();
-		
-		
+
 	}
 }
