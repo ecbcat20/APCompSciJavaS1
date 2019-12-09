15db3b58cb2c74758d6eec660f2a7bacecbd1463
184163 <184163@427-NA307-018.mcpsmd.org>
2019-12-09 12:48:32 -0500

    Started permutation

---
+++ APCS1/src/arrayList/Permutation.java
@@ -0,0 +1,18 @@
+package arrayList;
+
+import java.util.ArrayList;
+
+public class Permutation {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> list1 = new ArrayList<>();
+
+		for(int i = 0; i<10; i++) {
+			list1.add(i);
+		}
+		
+		System.out.println(list1);
+	}
+
+}
