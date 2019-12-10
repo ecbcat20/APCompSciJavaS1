8d4b0470ea65e2f3cf17e47005d295317af18e41
Skye <ecbcat20@gmail.com>
2019-12-09 20:51:06 -0500

    Finished permutation

--- APCS1/src/arrayList/Permutation.java
+++ APCS1/src/arrayList/Permutation.java
@@ -4,15 +4,40 @@ import java.util.ArrayList;
 
 public class Permutation {
 
+	public static ArrayList<Integer> permutate(ArrayList<Integer> l) {
+		ArrayList<Integer> list2 = l;
+		int rand;
+
+		for (int i = 0; i < 10; i++) {
+			rand = (int) (Math.random() * 10) + 0;
+			list2.add(l.get(rand));
+			l.remove(l.get(rand));
+		}
+
+		return list2;
+	}
+
 	public static void main(String[] args) {
 
 		ArrayList<Integer> list1 = new ArrayList<>();
 
-		for(int i = 0; i<10; i++) {
+		for (int i = 1; i <= 10; i++) {
 			list1.add(i);
 		}
+
+		System.out.println("Random Permutation Generator");
+		System.out.println("\n");
 		
-		System.out.println(list1);
+		System.out.println("List 1 " + permutate(list1));
+		System.out.println("List 2 " + permutate(list1));
+		System.out.println("List 3 " + permutate(list1));
+		System.out.println("List 4 " + permutate(list1));
+		System.out.println("List 5 " + permutate(list1));
+		System.out.println("List 6 " + permutate(list1));
+		System.out.println("List 7 " + permutate(list1));
+		System.out.println("List 8 " + permutate(list1));
+		System.out.println("List 9 " + permutate(list1));
+		System.out.println("List 10 " + permutate(list1));
 	}
 
 }
