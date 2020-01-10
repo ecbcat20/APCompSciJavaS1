b38ad89190bc56213a1a3da03ad5b216813e1a3e
184163 <184163@427-NA307-018.mcpsmd.org>
2020-01-10 12:48:14 -0500

    Added searchingandsorting stuff

---
+++ APCS1/src/searchingAndSorting/SearchTest.java
@@ -0,0 +1,27 @@
+package searchingAndSorting;
+
+public class SearchTest {
+
+	public static boolean binarySearch(double[] source, double key) {
+		int last = source.length - 1;
+		int first = 0;
+
+		int mid = (first + last) / 2;
+		int count = 0;
+		while (first <= last) {
+			count++;
+			if (source[mid] < key) {
+				first = mid + 1;
+			} else if (source[mid] > key) {
+				last = mid - 1;
+			} else {
+				System.out.println("compared " + count + " times");
+				return true;
+			}
+			mid = (first + last) / 2;
+		}
+		System.out.println("compared " + count + " times");
+		return false;
+	}
+
+}
